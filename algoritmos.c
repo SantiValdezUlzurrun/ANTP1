bc3a9fe32db9253e79f6324c0000a3b364cef245
Santiago Valdez <santiagoval1999@gmail.com>
2020-05-03 20:37:38 -0300

    Añadido primer algoritmo

---
+++ algoritmos.c
@@ -0,0 +1,10 @@
+#include "algoritmos.h"
+
+/* Algoritmo iterativo 1  */
+float algoritmo1(float Xk){
+
+	float montoDeLaCuenta = (CANTIDAD_DEPOSITADA / Xk) * (powf(1 + Xk, PERIODO_DE_DEPOSITO) - 1);
+	return  Xk - (montoDeLaCuenta - MONTO_DESEADO);
+
+	
+}
---
+++ algoritmos.h
@@ -0,0 +1,18 @@
+#ifndef ALGORITMOS_H_
+#define ALGORITMOS_H_
+
+
+#include <math.h>
+
+/* Constantes  */
+
+const int MONTO_DESEADO = 200000;
+const int PERIODO_DE_DEPOSITO = 30 * 12;
+const int NUMERO_DE_PADRON = 103783;
+const float CANTIDAD_DEPOSITADA = NUMERO_DE_PADRON/((float)700);
+
+/* Algorimtos  */
+float algoritmo1(float Xk);
+
+
+#endif /* ALGORITMOS_H_  */
